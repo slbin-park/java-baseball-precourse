a86a4e6330b1fdab8faef80f886376faf14129a5
slbin-park <smpts00@naver.com>
2022-10-25 13:50:33 +0900

    feat : 1~9 사이 중복되지 않는 난수 3개 생성 기능 추가 - Computer 클래스 생성 - 1~9 사이난수 3개 생성 함수 추가 - Computer 중복 체크 함수 추가

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,4 +1,27 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+
 public class Computer {
+    public ArrayList<Integer> threeRandomNumbers;
+    public void createThreeRandomNumbers(){
+        ArrayList<Integer> randomNumbers = new ArrayList<>();
+        while(randomNumbers.size()!=3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if( checkOverlap(randomNumbers,randomNumber)){
+                randomNumbers.add(randomNumber);
+            }
+        }
+        threeRandomNumbers = randomNumbers;
+    }
+    public boolean checkOverlap(ArrayList<Integer> numbers,Integer randomNumber){
+        for(int i : numbers){
+            if (i == randomNumber){
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -2,10 +2,17 @@ package baseball.domain;
 
 
 public class Game {
+    private static String GAME_START = "GAME START";
     private boolean status = true;
+    private Computer computer;
+    public Game(){
+        this.computer= new Computer();
+    }
     public void start(){
         while(status){
-            System.out.println("Game start");
+            System.out.println(GAME_START);
+            computer.createThreeRandomNumbers();
+            System.out.println(computer.threeRandomNumbers);
         }
     }
 }
